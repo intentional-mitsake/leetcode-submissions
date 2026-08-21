4a803b87885716ce4be0f6f453791b075867ee81
Gaurav <96043792+intentional-mitsake@users.noreply.github.com>
2026-08-21 09:46:49 +0545

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+    l, r := 0, len(nums)-1
+    
+    for l <= r {
+        m := (l + r)/2
+        if nums[m] == target {
+            return m
+        } else if nums[m] < target {
+            l = m + 1
+        } else {
+            r = m - 1
+        }
+    }
+    
+    return -1
+}
